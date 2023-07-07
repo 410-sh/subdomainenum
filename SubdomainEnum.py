d72919d3ba6772612aabc13905d75f71b8d41ca4
codered072 <kali@ip-172-31-80-144.ec2.internal>
2023-07-07 01:33:42 +0000

    implemented argparse

--- SubdomainEnum.py
+++ SubdomainEnum.py
@@ -1,22 +1,23 @@
+#!/usr/bin/python3
 from dns.resolver import resolve, NoAnswer
 import threading
 from progressbar import ProgressBar
+import argparse
 
-
-def get_input():
-    file_option = input(
-        "Select which file to use.\n(1) Small subdomain list\n(2) Medium subdomain list\n(3) Large subdomain list:  ")
-    if file_option == '1':
-        file_name = 'subdomains-small.txt'
-    elif file_option == '2':
-        file_name = 'subdomains-medium.txt'
-    elif file_option == '3':
-        file_name = 'subdomains-huge.txt'
-    else:
-        print('Invalid option.')
-        file_name = ""
-        get_input()
-    return file_name
+#def get_input():
+#    file_option = input(
+#        "Select which file to use.\n(1) Small subdomain list\n(2) Medium subdomain list\n(3) Large subdomain list:  ")
+#    if file_option == '1':
+#        file_name = 'subdomains-small.txt'
+#    elif file_option == '2':
+#        file_name = 'subdomains-medium.txt'
+#    elif file_option == '3':
+#        file_name = 'subdomains-huge.txt'
+#    else:
+#        print('Invalid option.')
+#        file_name = ""
+#        get_input()
+#    return file_name
 
 
 def scan_domain(file_name, domain):
@@ -39,8 +40,20 @@ def scan_domain(file_name, domain):
     for i in existing_urls:
         print(f'[+] {i}')
 
+try:
+    parser = argparse.ArgumentParser(
+            prog='',
+            description='simple subdommain enumeration tool',
+            add_help=True)
 
-domain = input('Enter the domain to scan: ').strip()
-file_name = get_input()
-x = threading.Thread(target=scan_domain, args=(file_name, domain,))
-x.start()
+    parser.add_argument('-u', type=str, required=True, help="URL to scan")
+    parser.add_argument('-w', type=str, required=True, help="Subdomain wordlist file")
+    options = parser.parse_args()
+
+    #domain = input('Enter the domain to scan: ').strip()
+    #file_name = get_input()
+    x = threading.Thread(target=scan_domain, args=(options.w, options.u,))
+    x.start()
+
+except KeyboardInterrupt:
+    print("\nStopped by user")
